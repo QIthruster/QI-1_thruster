88733ea4217be9fe0fe9c81acc4b48375bb40ecc
QIthruster <qi.thruster@gmail.com>
2022-09-09 21:54:52 +0100

    Updated 09.09.2022

---
+++ Functions.py
@@ -0,0 +1,200 @@
+#
+# QI thruster based on Raspberry Pi 4B
+#
+# Serial port interface (SPI) with the digital potentiometer:
+#   GPIO 7 - SPI CE1
+#   GPIO 11 - SPI CLK
+#   GPIO 10 - SPI MOSI
+#
+# The second SPI is reserved for ADC
+#
+# GPIO outputs:
+#   GPIO 21 - enable 15 V OpAmp
+#   GPIO 20 - red LED for ON/OFF of 15 V OpAmp
+#   GPIO 16 - red LED for low battery
+#   GPIO 19 - reserved or multiplexer CH1
+#   GPIO 26 - reserved or multiplexer CH2
+#
+# ADC inputs:
+#   In 9 - battery (1/3 potential divider and 5 V buffer)
+#   In 8 - 15 V OpAmp output (1/3 potential divider and 5 V buffer)
+#   In 7 - HV output (1/1000 potential divider and 5 V buffer)
+#   In 6 - reserved or sensor input (5 V buffer)
+#   In 5 - reserved or sensor input (5 V buffer)
+#   In 4 - reserved or sensor input (without buffer)
+#   In 3 - reserved or sensor input (without buffer)
+
+import spidev
+import time
+import ADS1263
+import RPi.GPIO as GPIO
+import os
+
+#GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Activating the digital potentiometer
+spi = spidev.SpiDev()
+spi.open(0, 1)
+spi.max_speed_hz = 976000
+
+# Activating ADC HAT
+REF = 5.0  # Reference voltage for ADC HAT  
+ADC = ADS1263.ADS1263()
+ADC.ADS1263_init()
+
+# Position of the digital potentiometer slider
+def write_pot(input):
+    msb = input >> 8
+    lsb = input & 0xFF
+    spi.xfer([msb, lsb])
+
+# Reading the voltage at the ADC channels
+def Read_ADC(Ch):
+    ADC_Value = ADC.ADS1263_GetAll()
+    try:
+        control = ADC_Value[1]
+    except:
+        print('Error with reading ADC. The program will be terminated')
+        exit()
+
+    Voltage = 0.0
+    try:
+        if(ADC_Value[Ch]>>31 ==1):
+            Voltage = REF * 2 - ADC_Value[Ch] * REF / 0x80000000
+        else:
+            Voltage = ADC_Value[Ch] * REF / 0x7fffffff
+    except:
+        print('Error with reading ADC. The program will be terminated')
+        exit()
+
+    return Voltage
+
+#    try:
+#        ADC_Value = ADC.ADS1263_GetAll()
+#        if(ADC_Value[Ch]>>31 ==1):
+#            Voltage = REF * 2 - ADC_Value[Ch] * REF / 0x80000000
+#        else:
+#            Voltage = ADC_Value[Ch] * REF / 0x7fffffff
+#        return Voltage
+#    except:
+#        exit()
+#    return Voltage
+
+# 15 Vcc Op Amp ON/OFF with the red LED indication
+def OpAmp_ES(output):
+    opamp = 21  # controlling op am amp output
+    LED = 20  # Red LED for op amp ON/OFF
+    GPIO.setup(opamp, GPIO.OUT)
+    GPIO.setup(LED, GPIO.OUT)
+    if output == 'ON':
+        GPIO.output(opamp, GPIO.HIGH)
+        GPIO.output(LED, GPIO.HIGH)
+    elif output == 'OFF':
+        GPIO.output(opamp, GPIO.LOW)
+        GPIO.output(LED, GPIO.LOW)
+
+# All GPIO outputs OFF
+def GPIO_OFF():
+    LED16 = 16  # Red LED for low battery
+    LED20 = 20  # Red LED for Op Amp
+    GPIO19 = 19  # reserved (or current sensor range 1)
+    GPIO26 = 26  # reserved (or current sensor range 2)
+    GPIO.setup(LED16, GPIO.OUT)
+    GPIO.setup(LED20, GPIO.OUT)
+    GPIO.setup(GPIO19, GPIO.OUT)
+    GPIO.setup(GPIO26, GPIO.OUT)
+    GPIO.output(LED16, GPIO.LOW)
+    GPIO.output(LED20, GPIO.LOW)
+    GPIO.output(GPIO19, GPIO.LOW)
+    GPIO.output(GPIO26, GPIO.LOW)
+
+# Controlling the battery charge
+def Battery():
+    LED = 16  # Red LED for the battery
+    GPIO.setup(LED, GPIO.OUT)
+    V = Read_ADC(9) * 3.0
+    print('Battery voltage = ', V, ' V')
+    if 6.0 <= V <= 6.5:
+        print('Warning: Battery is low!')
+        GPIO.output(LED, GPIO.HIGH)
+    elif V < 6.0:
+        print('Warning: Raspberry Pi will be shutdown in 5 s because the battery is dangerously low (< 6 V)')
+        time.sleep(5)
+        GPIO_OFF()  # All GPIO outputs OFF
+        write_pot(0x00)  # nulling the output of digital potentiometer
+        OpAmp_ES('OFF')  # OpAmp if OFF
+        os.system("shutdown now -h")
+
+# Voltage one step up
+def HV_up(Previous_NS, tau):
+
+    hex_Previous_NS = int(hex(Previous_NS), 16)
+    i = hex_Previous_NS
+    HV_actual = Read_ADC(7)  # actual HV across the capacitor
+
+    if i <= 0x101 and HV_actual <= 4.0:
+        i = i + 0x01
+        write_pot(i)
+        OpAmp = Read_ADC(8) * 6.0  # output of Op Amp (Vcc = 15 V)
+        time.sleep(tau)  # waiting for the capacitor charge
+        HV_actual = Read_ADC(7)
+    else:
+        OpAmp = Read_ADC(8) * 6.0  # output of Op Amp (Vcc = 15 V)
+
+    Battery()
+    print('Op Amp output = ', OpAmp, ' V')
+    print('Actual HV output = ', HV_actual, ' kV')
+
+    NS_stop = i
+            
+    return NS_stop, OpAmp, HV_actual
+
+# Voltage one step down
+def HV_down(Previous_NS, tau):
+    
+    hex_Previous_NS = int(hex(Previous_NS), 16)
+    i = hex_Previous_NS
+
+    if 0x00 < i:
+        i = i - 0x01
+        write_pot(i)
+        OpAmp = Read_ADC(8) * 6.0
+        time.sleep(tau)  # waiting for the capacitor discharge
+        HV_actual = Read_ADC(7)
+    else:
+        OpAmp = Read_ADC(8) * 6.0  # output of Op Amp (Vcc = 15 V)
+        HV_actual = Read_ADC(7)  # actual HV across the QI capacitor
+
+    Battery()
+    print('Op Amp output = ', OpAmp, ' V')
+    print('Actual HV output = ', HV_actual, ' kV')
+
+    NS_stop = i
+   
+    return NS_stop, OpAmp, HV_actual
+
+# Current sensor range
+def CurrentSensorRange(range):
+    range1 = 19
+    range2 = 26
+    GPIO.setup(range1, GPIO.OUT)
+    GPIO.setup(range2, GPIO.OUT)
+    GPIO.output(range1, GPIO.LOW)
+    GPIO.output(range2, GPIO.LOW)
+    if range == 1:
+        GPIO.output(range1, GPIO.HIGH)
+    elif range == 2:
+        GPIO.output(range2, GPIO.HIGH)
+
+# Current sensor CS-1
+# Sensor resistance 15.6 k (+/- 0.1%, low voltage)
+# Series resistance 1.5 MOhms (high voltage)
+# This sensor was calibrated with ADC In 3 without a buffer. The calibration file was saved on Raspberry Pi.
+def CurrentSensor(bias, Ch):
+    x = (Read_ADC(Ch) - bias)
+    y = -0.11372504497803 * x**8 + 1.4343168169319 * x**7 -7.3278668065487 * x**6 + \
+        19.4325718360446 * x**5 - 28.448512253201 * x**4 + \
+        22.3174566367283 * x**3 - 7.6319260686742 * x**2 + 1.1951000922818 * x
+    #return x  # used during the sensor calibration
+    return y
---
+++ main.py
@@ -0,0 +1,164 @@
+from Functions import *
+import numpy as np
+import time
+from pynput import keyboard
+from datetime import datetime
+
+# Measurement parameters (program inputs)
+R_series = 1.5  # resistance in series with the QI capacitor in MOhms
+R_shunt = 25.0  # shunting resistor across the QI capacitor in MOhms
+C = 0.01  # estimated QI capacitance in uF (micro Farads)
+factor_up = 5.0  # (factor_up * tau_up) is the waiting time for the capacitor full charge
+factor_down = 5.0  # (factor_down * tau_down) is the waiting time for the capacitor full discharge
+current_range = 1  # operational range of the current sensor; range 1 - GPIO 19, range 2 - GPIO 26
+Ch = 3  # Your channel (ADC In 3 - In 6) for the current sensor. In 5 and 6 are internally buffered.
+# The current sensor function CurrentSensor(bias, Ch) is in Functions.py
+
+# ****************************< Do not change anything below >*********************************************************
+# Rescaling the measurement parameters
+R_series = R_series * 1.0e+6  # MOhms to Ohms
+R_shunt = R_shunt * 1.0e+6  # MOhms to Ohms
+C = C * 1.0e-6  # uF to F
+
+# Initial configuration of the device
+GPIO_OFF()
+write_pot(0x00)  # nulling the output of digital potentiometer
+OpAmp_ES('OFF')  # OpAmp is OFF
+
+# Capacitor charging and discharging characteristic times
+tau_up = R_series * C  # charging characteristic time in seconds
+tau_down = (R_series + R_shunt) * C  # discharging characteristic time in seconds
+initial_discharge = factor_down * tau_down
+print('')
+print('Wait for the initial safety discharge of capacitor... ', int(initial_discharge), ' s')
+time.sleep(initial_discharge)
+print('')
+
+# Starting measurements
+print('Wait, we are calibrating the current sensor...')
+print('')
+# Calibration of the current sensor
+CurrentSensorRange(current_range)
+bias = 0.0
+for i in range(10):
+    bias = bias + Read_ADC(Ch)
+bias = bias / 10.0
+print('Bias voltage measured at the current sensor output = ', bias, ' V')
+print('')
+
+print('To stop the program, push the "Esc" button')
+print('To increase or decrease the voltage by one step push the "Shift" or "Ctrl" button respectively')
+print('After each push, wait for the indication!')
+print('You will also be prompted to manually enter the thrust value from the electronic scale')
+print('')
+
+Message = 'Enter the file name (without extension) in which you want to save the measurement data (or "q" to quit): '
+key = 1
+name = 'Failed_attempt'
+try:
+    name = str(input(Message))
+    if name == 'q' or name == 'Q':
+        GPIO_OFF()
+        write_pot(0x00)
+        OpAmp_ES('OFF')
+        key = 0
+        print('')
+except:
+    print('')
+    print('The program was terminated due to an invalid file name. Please try again')
+    GPIO_OFF()
+    write_pot(0x00)
+    OpAmp_ES('OFF')
+    exit()
+
+if key == 0:
+    exit()
+else:
+    # Ready for measurements
+    OpAmp_ES('ON')  # OpAmp is ON
+    Previous_NS = 0
+    HV_start = 0.0
+
+# Data and time stamp for the file name extension
+dateTimeObj = datetime.now()
+timestampStr = dateTimeObj.strftime("%d-%b-%Y %H:%M")
+
+folder = '/home/UoPi/Desktop/QI tests'  # The folder where all experiments are saved
+full_name = folder + '/' + name + '.txt'  # Full path to the file
+with open(full_name, 'w+') as file:
+    l1 = '# QI thruster based on Raspberry Pi 4B\n'
+    l2 = '# File name: ' + name + '.txt\n'
+    l3 = '# Date and time: ' + timestampStr + '\n'
+    l4 = '# Columns: ' + 'OpAmp output (V), HV output (kV), current (uA), thrust (your units)\n'
+    l5 = '# Note: Negative values must be treated as invalid\n'
+    l6 = '\n'
+    file.writelines([l1, l2, l3, l4, l5, l6])
+
+Message = 'Enter the thrust reading (any units) from the electronic scale: '
+with keyboard.Events() as events:
+    for event in events:
+        output = []
+        if event.key == keyboard.Key.esc:
+            print('The program has been terminated by your request.')
+            print('Warning: Wait until the capacitor is fully discharged before disconnecting it!')
+            GPIO_OFF()
+            write_pot(0x00)
+            OpAmp_ES('OFF')
+            Previous_NS = 0
+            HV_start = 0.0
+            file.close()
+            exit()
+        elif event.key == keyboard.Key.shift:
+            if str(event) == 'Press(key=Key.shift)':
+                NS_stop, OpAmp, HV_actual = HV_up(Previous_NS, factor_up * tau_up)
+                Previous_NS = NS_stop
+                Current = CurrentSensor(bias, Ch)
+                print('Current through the capacitor = ', Current, ' uA')
+                try:
+                    thrust = float(input(Message))
+                except:
+                    print('')
+                    print('You entered the number incorrectly')
+                    print('Try one more time... If incorrectly, the program will be terminated.')
+                    print('')
+                    try:
+                        thrust = float(input(Message))
+                    except:
+                        print('')
+                        print('The program was terminated due to an invalid value for the thrust')
+                        GPIO_OFF()
+                        write_pot(0x00)
+                        OpAmp_ES('OFF')
+                        file.close()
+                        exit()
+                output = np.column_stack([OpAmp, HV_actual, Current, thrust])
+                with open(full_name, "a+") as file:
+                    np.savetxt(file, output)
+                print('')
+        elif event.key == keyboard.Key.ctrl:
+            if str(event) == 'Press(key=Key.ctrl)':
+                NS_stop, OpAmp, HV_actual = HV_down(Previous_NS, factor_down * tau_down)
+                Previous_NS = NS_stop
+                Current = CurrentSensor(bias, Ch)
+                print('Current through the capacitor = ', Current, ' uA')
+                try:
+                    thrust = float(input(Message))
+                except:
+                    print('')
+                    print('You entered the number incorrectly')
+                    print('Try one more time... If incorrectly, the program will be terminated.')
+                    print('')
+                    try:
+                        thrust = float(input(Message))
+                    except:
+                        print('')
+                        print('The program was terminated due to an invalid value for the thrust')
+                        GPIO_OFF()
+                        write_pot(0x00)
+                        OpAmp_ES('OFF')
+                        file.close()
+                        exit()
+                output = np.column_stack([OpAmp, HV_actual, Current, thrust])
+                with open(full_name, "a+") as file:
+                    np.savetxt(file, output)
+                print('')
